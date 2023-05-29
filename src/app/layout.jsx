0020039f146d65a87874bdc119e951d826128d52
Devin Reilly <werthird2016@gmail.com>
2023-05-29 00:35:31 -0400

    added style and content to projects page

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -16,7 +16,8 @@ import Header from './components/Header'
 // Font
 const inter = Inter({ 
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-  subsets: ['latin'] })
+  subsets: ['latin'] 
+})
 
 // Metadata
 export const metadata = {
--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -2,11 +2,50 @@
 //                      PROJECT PAGE
 // =====================================================
 
+import Image from 'next/image';
+import './style.css'
+import Link from 'next/link';
+import { BsGithub } from 'react-icons/bs';
+import { BsFillArrowUpRightSquareFill } from 'react-icons/bs';
+
 
 export default function Home() {
   return (
-    <div className='main-div'>
-      <h1 className=''>Projects</h1>
+    <div className='project-div'>
+      <h1 className='slide-in-left'>Projects</h1>
+      <div className='underline-div slide-in-right'></div>
+      {/* <Image src={lightbulbPic} width='150' alt='a lightbulb icon'/> */}
+      <div className='featured-project'>
+        <div className='featured-image'><Image src='/images/404-gif.gif' width={575} height={0} alt='404 image' /></div>
+        <div className='featured-content flex-end-items'>
+          <p className='featured-content-label'>Featured Project</p>
+          <h3 className='featured-content-title'>Aardvarchive</h3>
+          <div className='featured-content-desc flex-end-content'><p>Some project description</p></div>
+          <p className='featured-content-tech'><span>Tech</span><span>Tech</span><span>Tech</span><span>Tech</span><span>Tech</span></p>
+          <p className='featured-content-link'><Link href='./'><BsGithub className='github-icon' /></Link><Link href='./'><BsFillArrowUpRightSquareFill className='deployed-site-icon' /></Link></p>
+        </div>
+      </div>
+      <div className='featured-project'>
+        <div className='featured-content'>
+          <p className='featured-content-label'>Featured Project</p>
+          <h3 className='featured-content-title'>Tech Blog</h3>
+          <div className='featured-content-desc'><p>Some project description</p></div>
+          <p className='featured-content-tech'><span>Tech</span><span>Tech</span><span>Tech</span><span>Tech</span><span>Tech</span></p>
+          <p className='featured-content-link'><Link href='./'><BsGithub className='github-icon' /></Link><Link href='./'><BsFillArrowUpRightSquareFill className='deployed-site-icon' /></Link></p>
+        </div>
+        <div className='featured-image'><Image src='/images/404-gif.gif' width={575} height={0} alt='404 image' /></div>
+      </div>
+      <div className='featured-project'>
+        <div className='featured-image'><Image src='/images/404-gif.gif' width={575} height={0} alt='404 image' /></div>
+        <div className='featured-content flex-end-items'>
+          <p className='featured-content-label'>Featured Project</p>
+          <h3 className='featured-content-title'>Project Name</h3>
+          <div className='featured-content-desc flex-end-content'><p>Some project description</p></div>
+          <p className='featured-content-tech'><span>Tech</span><span>Tech</span><span>Tech</span><span>Tech</span><span>Tech</span></p>
+          <p className='featured-content-link'><Link href='./'><BsGithub className='github-icon' /></Link><Link href='./'><BsFillArrowUpRightSquareFill className='deployed-site-icon' /></Link></p>
+        </div>
+      </div>
+
     </div>
   );
 };
