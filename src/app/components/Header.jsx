5cb08fd2eee216edbddcca64bb8264ae5aeba46d
Devin Reilly <werthird2016@gmail.com>
2023-06-03 16:58:40 -0400

    adjusted header for mobile responsive

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,46 +1,68 @@
+'use client';
+
+
 import './style.css'
 import Link from 'next/link';
 import Image from 'next/image';
+import { useState } from 'react';
 
 
 export default function Header() {
+
+  const [display, setDisplay] = useState(true);
+
   return (
     <div className='header-layout'>
       <div className='header-image-wrapper fade-in'>
        <Image src='/images/logo-removebg.png' width='175' height='175' alt='Devin Reilly logo' className='logo-image'/>
       </div>
-      <nav className='navbar'>
-        <div className='nav-link fade-in animation-delay-02'>
-          <Link href='./' > 
-            HOME
-          </Link>
-        </div>
-        
-        <div className='nav-link fade-in animation-delay-03'>
-          <Link href='./about'>
-            ABOUT
-          </Link>
+
+      <div className='menu-toggle' onClick={() => setDisplay(!display)}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+
+      <div className='navbar-container' style={{ display: display ? "block" : "none" }}>
+
+        <div className='navbar-exit' onClick={() => setDisplay(!display)}>
+          <div className='exit-div-1'></div>
+          <div className='exit-div-2'></div>
         </div>
 
-        <div className='nav-link fade-in animation-delay-04'>
-          <Link href='./projects'>
-            PROJECTS
-          </Link>
-        </div>
+        <nav className='navbar'>
+          <div className='nav-link fade-in animation-delay-02'>
+            <Link href='./' > 
+              HOME
+            </Link>
+          </div>
+          
+          <div className='nav-link fade-in animation-delay-03'>
+            <Link href='./about'>
+              ABOUT
+            </Link>
+          </div>
 
-        <div className='nav-link fade-in animation-delay-05'>
-          <Link href='./contact'>
-            CONTACT
-          </Link>
-        </div>
+          <div className='nav-link fade-in animation-delay-04'>
+            <Link href='./projects'>
+              PROJECTS
+            </Link>
+          </div>
 
-        <div className='nav-link fade-in animation-delay-06'>
-          <Link href='https://github.com/werthird' target='_blank'>
-            GITHUB
-          </Link>
-        </div>
+          <div className='nav-link fade-in animation-delay-05'>
+            <Link href='./contact'>
+              CONTACT
+            </Link>
+          </div>
 
-      </nav>
+          <div className='nav-link fade-in animation-delay-06'>
+            <Link href='https://github.com/werthird' target='_blank'>
+              GITHUB
+            </Link>
+          </div>
+
+        </nav>
+      </div>
     </div>
   );
 };
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -17,7 +17,7 @@ import './homeStyles.css'
 export default function Home() {
   return (
     <div className='main-div'>
-      <h1 className='main-h1 fade-in animation-delay-06'>Welcome.</h1>
+      <h1 className='main-h1 fade-in animation-delay-04'>Welcome.</h1>
       <div className='main-content fade-in animation-delay-06'>
         <p className='fade-in animation-delay-08'>My name is <span className='text-emph'>Devin Reilly</span>, I'm a Full-Stack Web Developer based out of South Florida. I have developed many web applications, from frontend UI to backend database management. </p>
         <br />
